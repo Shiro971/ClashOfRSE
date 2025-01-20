b474aa2773ec560523ec40eaa8b5696507076dc3
Ibtissam78 <ibtissamelhany.pro@gmail.com>
2025-01-20 21:41:11 +0100

    V1 pas le final

--- src/views/auth/Profil.js/index.js
+++ src/views/auth/Profil.js/index.js
@@ -96,15 +96,15 @@ export default function Profile() {
                 </div>
                 <div className="text-center mt-12">
                   <h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-                    Jenna Stones
+                    Julien Jones 
                   </h3>
                   <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
                     <i className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>{" "}
-                    Los Angeles, California
+                    France, Paris
                   </div>
                   <div className="mb-2 text-blueGray-600 mt-10">
                     <i className="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-                    Solution Manager - Creative Tim Officer
+                    Responsable des Solutions - Directeur Créatif chez Tim
                   </div>
                   <div className="mb-2 text-blueGray-600">
                     <i className="fas fa-university mr-2 text-lg text-blueGray-400"></i>
