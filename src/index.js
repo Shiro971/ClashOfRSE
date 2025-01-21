1bffe8567830358638f726c18b5199add85ade2e
Ibtissam78 <ibtissamelhany.pro@gmail.com>
2025-01-21 07:09:44 +0100

    V1 pas le final

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ import Auth from "layouts/Auth.js";
 import Landing from "views/Landing.js";
 import Profile from "views/Profile/Profile.js";
 import Index from "views/Index.js";
-import DefiForm from "views/Compte Admin/Défis/DefiForm.js"; // Ajoute cette ligne pour importer DéfisFormulaire
-
+import DefiForm from "views/Compte Admin/Défis/DefiForm";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
