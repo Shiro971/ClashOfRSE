23f82eb5039d508f4e53a26b5f3a55e32902803b
Ibtissam78 <ibtissamelhany.pro@gmail.com>
2025-01-21 10:25:53 +0100

    V1 pas le final

--- src/views/Compte Admin/ProfileCustomizationForm/ProfileCustomizationForm.js
+++ src/views/Compte Admin/ProfileCustomizationForm/ProfileCustomizationForm.js
@@ -134,7 +134,7 @@ const ProfileCustomizationPage = () => {
                             className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
                             onClick={() => setSubmittedData(null)}
                           >
-                            Personnaliser un autre profil
+                           
                           </button>
                         </div>
                       )}
